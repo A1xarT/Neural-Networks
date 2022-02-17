1d6196ccb02181774f64c076c0dd50bb5cb0f0ed
A1xarT <illya030778@gmail.com>
2022-02-17 14:58:09 +0200

    1

--- lab1/main.py
+++ lab1/main.py
@@ -6,6 +6,6 @@ def convert_all_images(number):
 
 
 if __name__ == '__main__':
-    # convert_all_images(65)
-    print()
-    pixelizer.count_points()
+    #convert_all_images(65)
+    points = pixelizer.get_keypoints()
+    pixelizer.set_keypoints(points, 'Dataset/Training_set/Without_points_rgb', '1.jpg', 'Dataset/Training_set/Trash')
--- lab1/pixelizer.py
+++ lab1/pixelizer.py
@@ -1,25 +1,36 @@
 from PIL import Image
 from os import listdir
 from os.path import isfile, join
+import os
 
 
-def get_pixels():
-    return Image.open('Dataset/With_points/1.jpeg').load()
+def get_keypoints():
+    points = []
+    keypoint_color = (237, 28, 36, 255)
+    img = Image.open('Dataset/Training_set/With_points/1.png')
+    pix = img.load()
+    size = 100
+    for j in range(size):
+        for i in range(size):
+            if pix[i, j] == keypoint_color:
+                points.append((i, j))
+    return points
 
 
-def count_points():
-    total_points = 0
-    img = Image.open('Dataset/Training_set/With_points/1.png', 'r')
-    pixel_values = list(img.getdata())
-    for i in pixel_values:
-        if i[0] == 237 and i[1] == 28 and i[2] == 36:
-            total_points += 1
-    print(total_points)
+def set_keypoints(points, folder_name, img_name, new_folder_name):
+    keypoint_color = (237, 28, 36, 255)
+    img = Image.open(folder_name + '/' + img_name)
+    pix = img.load()
+    for i in points:
+        pix[i[0], i[1]] = keypoint_color
+    # img.show()
+    img.save(new_folder_name + '/' + os.path.splitext(img_name)[0] + '.png')
 
 
 def convert_image(filename):
-    Image.open(f'Dataset/Training_set/Default_set/{filename}').resize((100, 100)).convert("L").save(
-        f'Dataset/Training_set/Without_points/{filename}')
+    img = Image.open(f'Dataset/Training_set/Default_set/{filename}').resize((100, 100))
+    img.save(f'Dataset/Training_set/Without_points_rgb/{filename}')
+    img.convert("L").save(f'Dataset/Training_set/Without_points/{filename}')
 
 
 def get_all_filenames(mypath):
